8a4c139736e4383fa53d7a1bfebc94c3028f7679
jeisonj2019 <jeisonj2019@gmail.com>
2023-02-19 21:27:38 +0500

    Initial commit

---
+++ Models/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudioMgn.Models
+{
+    public class Appointment
+    {
+        public string Name { get; set; }
+        [Required]
+        public DateTime Start { get; set; }
+        [Required]
+        public DateTime End { get; set; }
+        [Phone]
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public AppointmentType? Type { get; set; }
+        public string? Comment { get; set; }
+        public string Description { get; set; }
+    }
+   public enum AppointmentType
+    {
+        ЗАПИСЬ,
+        ИНДИВИДУАЛЬНОЕ_ЗАНЯТИЕ,
+        УРОК,
+        РЕПЕТИЦИЯ_ГРУППЫ
+    }
+}
